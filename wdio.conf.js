8c6813d87265b0cbc6658e9446f12c4a0aecc979
André Mendes <andre.mendes@concrete.com.br>
2018-09-28 17:18:38 -0300

    Minor adjustments & Report

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,67 +1,70 @@
 const waitforTimeout = 30 * 60000;
 const commandTimeout = 30 * 60000;
 
-const host = '127.0.0.1'; 
-const port = 4730;  
+const host = "127.0.0.1";
+const port = 4730;
 
 exports.config = {
+  specs: ["./features/**/*.feature"],
 
-    specs: [
-        './features/**/*.feature'
-    ],
+  reporters: ["spec", "mochawesome"],
 
-    reporters: ['spec'],
-    host: host,
-    port: port,
-    maxInstances: 1,
-    capabilities: [
-        {
-            appiumVersion: '1.8.1',
-            browserName: '',
-            platformName: 'Android',
-            app: './apk/TrianguloApp.apk',
-            appPackage: 'com.eliasnogueira.trianguloapp',
-            appActivity: 'com.eliasnogueira.trianguloapp.MainActivity',
-            deviceName: 'Android Emulator',
-            waitforTimeout: waitforTimeout,
-            commandTimeout: commandTimeout,
-        }
-    ],
+  reporterOptions: {
+    outputDir: "./",
+    mochawesome_filename: "report.json"
+  },
 
-    logLevel: 'silent',
-    screenshotPath: './errorShots/',
-    connectionRetryCount: 3,
-    services: ['appium'],
-    appium: {
-        waitStartTime: 6000,
-        waitforTimeout: waitforTimeout,
-        command: 'appium',
-        logFileName: 'appium.log',
-        args: {
-            address: host,
-            port: port,
-            commandTimeout: commandTimeout,
-            sessionOverride: true,
-            debugLogSpacing: true
-        }
-    },
+  host: host,
+  port: port,
+  maxInstances: 1,
+  capabilities: [
+    {
+      appiumVersion: "1.8.1",
+      browserName: "",
+      platformName: "Android",
+      app: "./apk/TrianguloApp.apk",
+      appPackage: "com.eliasnogueira.trianguloapp",
+      appActivity: "com.eliasnogueira.trianguloapp.MainActivity",
+      deviceName: "Android Emulator",
+      waitforTimeout: waitforTimeout,
+      commandTimeout: commandTimeout
+    }
+  ],
 
-    framework: 'cucumber',
+  logLevel: "silent",
+  screenshotPath: "./errorShots/",
+  connectionRetryCount: 3,
+  services: ["appium"],
+  appium: {
+    waitStartTime: 6000,
+    waitforTimeout: waitforTimeout,
+    command: "appium",
+    logFileName: "appium.log",
+    args: {
+      address: host,
+      port: port,
+      commandTimeout: commandTimeout,
+      sessionOverride: true,
+      debugLogSpacing: true
+    }
+  },
 
-    cucumberOpts: {
-        require: ['./features/step-definitions/*'],
-        backtrace: false,
-        compiler: [],
-        dryRun: false,
-        failFast: false,
-        format: ['pretty'],
-        colors: false,
-        snippets: false,
-        source: true,
-        profile: [],
-        strict: true,
-        tagExpression: [],
-        timeout: 50000,
-        ignoreUndefinedDefinitions: true,
-    },
-}
+  framework: "cucumber",
+
+  cucumberOpts: {
+    require: ["./features/step-definitions/*"],
+    backtrace: false,
+    compiler: [],
+    dryRun: false,
+    failFast: false,
+    format: ["pretty"],
+    colors: false,
+    snippets: false,
+    source: true,
+    profile: [],
+    strict: true,
+    tagExpression: [],
+    timeout: 50000,
+    ignoreUndefinedDefinitions: true
+  }
+};
